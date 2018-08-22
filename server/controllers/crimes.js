c90cc98f2779ef88fb3771cded805162919d3e08
chen3e <ch3n3e@gmail.com>
2018-08-22 15:16:40 -0500

    Database included! It should be in the outer project folder.

--- server/controllers/crimes.js
+++ server/controllers/crimes.js
@@ -1,11 +1,12 @@
 const unirest = require('unirest');
-const io = require('socket.io');
-const server = require("../../server");
-console.log(server);
+// const io = require('socket.io');
+// const server = require("../../server");
+// console.log(server);
+const mongoose = require('mongoose');
+Crime = mongoose.model('Crime')
 
 module.exports = {
     getCrimes: function(req, res, io) {
-<<<<<<< HEAD
         // // These code snippets use an open-source library. http://unirest.io/nodejs
         // unirest.get("https://yourmapper2.p.mashape.com/markers?center=0&f=json&id=182&lat=41.881&lon=-87.623&num=500&start=2015-11-22&end=2015-11-22")
         // // Put api key here
@@ -17,18 +18,6 @@ module.exports = {
         //     return res.json({ message: "Success!", data: result.body });
         // });
         
-=======
-        // These code snippets use an open-source library. http://unirest.io/nodejs
-        unirest.get("https://yourmapper2.p.mashape.com/markers?center=0&f=json&id=182&lat=41.881&lon=-87.623&num=500&start=2015-11-22&end=2015-11-22")
-        // Put api key here
-        .header("X-Mashape-Key", "b9cHBlr2PUmsh98hyrF30fUgJhNFp1d4QMFjsng2zNpmyY0H0C")
-        .header("Accept", "application/json")
-        .end(function (result) {
-            console.log(result.status, result.headers);
-            // io.emit("crime_data", { message: "Success!", data: result.body })
-            return res.json({ message: "Success!", data: result.body });
-        });
->>>>>>> 5222692389b5ffbad4f0a0888f54187844edf1f5
     },
 
     filterCrimes: function(req, res) {
@@ -57,6 +46,39 @@ module.exports = {
             console.log(result.status, result.headers);
             return res.json({ message: "Success!", data: result.body });
         });
+    },
+
+    //SANITY CHECK TO MAKE SURE ANY GIVEN DATE HAS CRIMES
+    findCrimeForMe(req, res) {
+       console.log("In getCrimeForMe");
+       var crime = Crime.findOne({date: "2015-11-05"}, function(err, crime){
+           if (err){
+               console.log("???")
+           }
+           else{
+               console.log(crime);
+           }
+       });
+    },
+
+    makeCrimes(req, res) {
+        console.log(req.body);
+        console.log("Made it to server!");
+        for (var i = 0; i < req.body.length; i++){
+            console.log(req.body[i]);
+            var crime = new Crime({address: req.body[i].address, catcolor: req.body[i].catcolor, categoryid: req.body[i].categoryid, catimage: req.body[i].catimage, catname: req.body[i].catname, city: req.body[i].city, date: req.body[i].date, description: req.body[i].description, distance: req.body[i].distance, geoid: req.body[i].geoid, id: req.body[i].id,latitude: req.body[i].latitude, longitude: req.body[i].longitude, name: req.body[i].name, permalink: req.body[i].permalink, state: req.body[i].state})
+            crime.save(function(err, crime){
+                //console.log("********************************************************* Trying to save! ********************************************************")
+                if (err){
+                    console.log("Eek!")
+                }
+                else{
+                    console.log("Cool!")
+                    console.log(crime);
+                }
+            })
+        }
+        res.json({ok: true});
     }
 }
 
