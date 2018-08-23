a76e74426e652fe293d3fb64d386220377fa5bcf
Jeremy <jveto91@gmail.com>
2018-08-23 13:31:46 -0500

    Graph work

--- public/src/app/graph/graph.component.ts
+++ public/src/app/graph/graph.component.ts
@@ -9,17 +9,49 @@ import { Chart } from 'chart.js';
 })
 export class GraphComponent implements OnInit {
   LineChart = [];
+  graph1 = [];
   searchParams = {
     keyword: null,
     categoryid: null,
     region: null,
     date: null
   };
+  crimeLabels = [];
+  crimeDict = {
+    "1": "Battery",
+    "2": "Assault",
+    "3": "Motor Vehicle Theft",
+    "4": "Theft",
+    "5": "Criminal Damage",
+    "6": "Burglary",
+    "7": "Sex Offense",
+    "8": "Robbery",
+    "9": "Miscellaneous",
+    "10": "Sexual Assault",
+    "11": "Deceptive Practice",
+    "12": "Weapons Violation",
+    "13": "Narcotics",
+    "14": "Prostitution",
+    "15": "Arson",
+    "16": "Homicide",
+    "17": "Gambling",
+    "18": "Kidnapping",
+    "19": "Obscenity",
+    "20": "Intimidation",
+    "21": "Stalking"
+  }
+  precrimeLabels = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+  dataCrimeArray = [];
+  crimeData = [];
   crimes: any;
+  showSearch: Boolean;
+  searchMessage = "Filter Results";
   constructor(private _httpService: HttpService) { }
   ngOnInit() {
-    this.initChart();
-    this.initChart2();
+    //this.initChart();
+    //this.initChart2();
+    //this.initPieChart();
+    this.showSearch = false;
   }
   initChart() {
     this.LineChart = new Chart('lineChart', {
@@ -80,15 +112,76 @@ export class GraphComponent implements OnInit {
       }
     });
   }
+  initPieChart() {
+    if (canvas) {
+      console.log('it exisits');
+    }
+    console.log("making pie");
+    console.log(this.crimeLabels);
+    console.log(this.crimeData);
+    var canvas = <HTMLCanvasElement>document.getElementById('pieChart');
+    var ctx = canvas.getContext('2d');
+    var myPieChart = new Chart(ctx, {
+      type: 'pie',
+      data: {
+        datasets: [{
+          data: this.crimeData,
+          backgroundColor: ['red', 'yellow', 'blue', 'green'],
+          borderWidth: 0
+          
+        }],
+        labels: this.crimeLabels
+      },
+      options: {
+      }
+    })
+  }
   filterCrimes() {
+    this.crimeLabels = [];
+    this.precrimeLabels = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    this.dataCrimeArray = [];
+    this.crimeData = [];
     console.log("In filter");
     let observable = this._httpService.filterCrimes(this.searchParams);
     observable.subscribe(data => {
-      console.log(data);
-      console.log("Crimes:")
+      console.log("Here were the entered search params");
+      console.log(this.searchParams);
       this.crimes = data["data"];
+      
+      for (var i = 0; i < data['data'].length; i++) {
+        //console.log(data['data'][i]);
+        //console.log('this is the category id', data['data'][i]['categoryid']);
+        //console.log('this is the dict data of that id', this.crimeDict[data['data'][i]['categoryid']]);
+        //console.log(this.crimeLabels[data['data'][i]['categoryid']]);
+        this.precrimeLabels[data['data'][i]['categoryid']]++;
+        //console.log(this.crimeLabels);
+      }
+      for (var j = 1; j < this.precrimeLabels.length; j++) {
+        this.dataCrimeArray.push(this.precrimeLabels[j]);
+      }
+      for (var k = 0; k < this.dataCrimeArray.length; k++) {
+        if (this.dataCrimeArray[k] != 0) {
+          this.crimeLabels.push(this.crimeDict[k + 1]);
+          this.crimeData.push(this.dataCrimeArray[k]);
+        }
+      }
+      console.log(this.dataCrimeArray);
+      console.log(this.crimeLabels);
       console.log("Here are some graph crimes");
       console.log(this.crimes);
+      this.initPieChart();
     })
+  }
+  show() {
+    if (!this.showSearch) {
+      console.log("Showing")
+      this.showSearch = true;
+      this.searchMessage = "Hide Search";
+    }
+    else {
+      console.log("Hiding")
+      this.showSearch = false;
+      this.searchMessage = "Filter Results"
+    }
   }
 }
