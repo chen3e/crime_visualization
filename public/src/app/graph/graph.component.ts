67a3cdcc86b3f582bd33114a96144dd5e922deb1
Jeremy <jveto91@gmail.com>
2018-08-22 21:10:28 -0500

    Progress on graph

--- public/src/app/graph/graph.component.ts
+++ public/src/app/graph/graph.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpService } from '../http.service';
 import { Chart } from 'chart.js';
 
 @Component({
@@ -8,8 +9,14 @@ import { Chart } from 'chart.js';
 })
 export class GraphComponent implements OnInit {
   LineChart = [];
-
-  constructor() { }
+  searchParams = {
+    keyword: null,
+    categoryid: null,
+    region: null,
+    date: null
+  };
+  crimes: any;
+  constructor(private _httpService: HttpService) { }
   ngOnInit() {
     this.initChart();
     this.initChart2();
@@ -18,9 +25,12 @@ export class GraphComponent implements OnInit {
     this.LineChart = new Chart('lineChart', {
       type: 'line',
       data: {
+        
         labels: ['jan', 'feb', 'march', 'april', 'may', 'june', 'july', 'aug', 'sep', 'oct', 'nov', 'dec'],
         datasets: [{
+          
           label: 'Number of items sold in months',
+          
           data: [9, 2, 4, 15, 42, 88, 100, 5],
           fill: false,
           lineTension: 0.2,
@@ -49,15 +59,36 @@ export class GraphComponent implements OnInit {
     var chart = new Chart(ctx, {
       type: 'line',
       data: {
+        //labels for x axis - Dataset data will be at each point on this array - if this array is shorter
+        // than the data array, it will leave the data points off that go beyond label array length
         labels: ['jan', 'feb', 'mar', 'april', 'may', 'june', 'july'],
         datasets: [{
+          //title/legend?
           label: "My First dataset",
           backgroundColor: 'pink',
           borderColor: 'blue',
+          //this is the data we want to provide - ideally array length is same size as labels array
           data: [2, 5, 6, 78, 4, 6]
         }]
       },
-      options: {}
+      options: {
+        hover: {
+          mode: 'nearest',
+          intersect: false,
+
+        }
+      }
     });
+  }
+  filterCrimes() {
+    console.log("In filter");
+    let observable = this._httpService.filterCrimes(this.searchParams);
+    observable.subscribe(data => {
+      console.log(data);
+      console.log("Crimes:")
+      this.crimes = data["data"];
+      console.log("Here are some graph crimes");
+      console.log(this.crimes);
+    })
   }
 }
--- server/controllers/crimes.js
+++ server/controllers/crimes.js
@@ -14,7 +14,7 @@ module.exports = {
         //     // io.emit("crime_data", { message: "Success!", data: result.body })
         //     return res.json({ message: "Success!", data: result.body });
         // });
-        Crime.find({ name:'HOMICIDE'}, function(err, data) {
+        Crime.find({ date:'2015-11-22'}, function(err, data) {
             if (err) {
                 console.log(err);
             }
