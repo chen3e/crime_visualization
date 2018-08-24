4f86ec28b020ffef9da9e0fb76a6ef02132eb61c
Jeremy <jveto91@gmail.com>
2018-08-24 11:24:34 -0500

    all working

--- server/config/routes.js
+++ server/config/routes.js
@@ -2,7 +2,7 @@ const crimes = require("../controllers/crimes")
 const path = require("path")
 
 module.exports = function (app) {
-    app.get("/crimes", function(req, res) {
+    app.get("/crimes", function (req, res) {
         crimes.getCrimes(req, res);
     })
 
@@ -10,11 +10,11 @@ module.exports = function (app) {
         crimes.findCrimesCount(req, res);
     })
 
-    app.post("/crimes", function(req, res) {
+    app.post("/crimes", function (req, res) {
         crimes.filterCrimes(req, res);
     })
 
-    app.post("/reportCrime", function(req, res) {
+    app.post("/reportCrime", function (req, res) {
         crimes.reportCrime(req, res);
     })
 
@@ -24,10 +24,10 @@ module.exports = function (app) {
 
     // SANITY CHECK TO SEE IF CRIMES EXIST ON ANY GIVEN DAY- EDIT CRIMES.JS TO CHANGE THE DAY
     app.get("/findCrimeForMe", (req, res) => {
-       console.log("in findcrimeforme");
-       crimes.findCrimeForMe(req, res);
+        console.log("in findcrimeforme");
+        crimes.findCrimeForMe(req, res);
     })
-    
+
     app.all("*", (req, res, next) => {
         res.sendFile(path.resolve("./public/dist/public/index.html"))
     });
